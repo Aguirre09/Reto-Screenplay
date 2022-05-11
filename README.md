c16c8821e0f7aff5598a6fc6ae2acd26403afeb4
alejo609 <alejo609@gmail.com>
2022-05-11 15:11:51 -0500

    project with gitignore

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Reto-Screenplay
+##Automation Challenge Screenplay: applicants to QA
+
+#### Sistecredito's challenge
+
+Project where is used serenity screenplay pattern with cucumber and gradle.
+
+Author : Ing. Luis Alejandro Aguirre Sossa
---
+++ src/main/java/co/com/reto/choucair/sistecredito/tasks/AddItem.java
@@ -0,0 +1,32 @@
+package co.com.reto.choucair.sistecredito.tasks;
+
+import co.com.reto.choucair.sistecredito.userinterface.IndexPage;
+import co.com.reto.choucair.sistecredito.utils.Constants;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+
+import static co.com.reto.choucair.sistecredito.userinterface.PurchasePage.BUTTON_ADD_CAR_MARKET;
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+public class AddItem implements Task {
+    private  String item;
+
+    public AddItem(String item) {
+        this.item = item;
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T theActor) {
+        theActor.attemptsTo(
+                Enter.theValue(item).into(IndexPage.ITEM_PLACE),
+                Click.on(IndexPage.BUTTON_SEARCH),
+                Click.on(BUTTON_ADD_CAR_MARKET)
+        );
+    }
+
+    public static AddItem addItem(String item){
+        return instrumented(AddItem.class,item);
+    }
+}
